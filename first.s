502be22feafee07ac45d4420cfa0320bba573c46
AndiCucka <124384763+AndiCucka@users.noreply.github.com>
2023-10-04 16:40:42 +0000

    Comment

---
+++ first.s
@@ -0,0 +1,8 @@
+1 /* -- first.s */
+2 /* This is a comment */
+3 .global main /* entry point must be global */
+4 .func main /* ’main’ is a function */
+5
+6 main: /* This is main */
+7 mov r0, #2 /* Put a 2 into register r0 */
+8 bx lr /* Return from main */.andi
